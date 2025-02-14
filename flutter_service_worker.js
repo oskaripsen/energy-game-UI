8cacf946bbe5a00f65aa5906dca32be582212c40
oskaripsen <oskaripsen@users.noreply.github.com>
2025-02-14 16:51:52 +0000

    deploy: 4a1aa10fbad1421ab13e93bd8763ad0ce49eecd7

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -14,7 +14,7 @@ const RESOURCES = {"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb0489
 "canvaskit/skwasm.js": "1df4d741f441fa1a4d10530ced463ef8",
 "canvaskit/canvaskit.wasm": "f48eaf57cada79163ec6dec7929486ea",
 "canvaskit/canvaskit.js": "76f7d822f42397160c5dfc69cbc9b2de",
-"main.dart.js": "5e0338d85104a6d55d5b0185bbe19506",
+"main.dart.js": "1c5bd034384e66ca0849ac527f268e92",
 "version.json": "9f68c899fefac7882b5bd5d5da405596",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/shaders/ink_sparkle.frag": "f8b80e740d33eb157090be4e995febdf",
